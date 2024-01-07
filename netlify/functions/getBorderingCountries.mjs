54213df696f32bbe9f062d81c651168c352abe8e
fazzaamiarso <fazzaamiarso@gmail.com>
2024-01-07 20:27:28 +0700

    fix: change buggy Promises.all on Netlify functions with endpoint

--- netlify/functions/getBorderingCountries.mjs
+++ netlify/functions/getBorderingCountries.mjs
@@ -5,27 +5,21 @@ export default async (req) => {
     const countryCodes = params[0].split(",")
 
     try {
-        const promiseArray = countryCodes.map((code) => {
-            return countryClient.get(`alpha/${code.toLowerCase()}`, {
-                params: {
-                    fields: ["name", "cca3"],
-                },
-                paramsSerializer: { indexes: null },
-            });
+        const res = await countryClient.get(`alpha`, {
+            params: {
+                codes: countryCodes
+            },
+            paramsSerializer: { indexes: null },
+
         })
-        const promises = await Promise.all(promiseArray);
 
-        console.log("PARAMS:", JSON.stringify(params))
-        console.log("PROMISES ARRAY:", JSON.stringify(promiseArray))
-        console.log("PROMISES:", JSON.stringify(promises))
 
-        const data = promises.map((country) => ({
-            name: country.data.name.common,
-            code: country.data.cca3.toLowerCase(),
+        const data = res.data.map((country) => ({
+            name: country.name.common,
+            code: country.cca3.toLowerCase(),
         }));
-        console.log("BORDERS:", data)
         return Response.json(data);
     } catch (e) {
-        throw new Error("Something went wrong!");
+        throw new Error(e);
     }
 }
